2e188b1a205b273495987dc7a2080a07ed16b720
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-02 01:09:46 +0600

    Add Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
+import banner from '../assets/images/banner1.avif'
 
 const Home = () => {
     return (
-        <div>
+        <>
+            <div className="h-[89vh] bg-no-repeat bg-cover flex items-center" style={{ backgroundImage: `url(${banner})` }}>
+                <div className='text-white px-2 md:px-10 align-middle md:w-1/2'>
+                    <h1 className='text-6xl  mb-4'>𝑪𝒉𝒆𝒇&lsquo;𝒔 𝑫é𝒍𝒊𝒄𝒊𝒆𝒖𝒙</h1>
+                    <p className='text-xl mb-6'>Explore the authentic French cuisine with our curated collection of recipes from the top French chefs.</p>
+                    <button className="bg-red-800 text-white text-center px-6 py-3 rounded font-semibold">Discover Chefs</button>
+                </div>
+            </div>
             
-        </div>
+            <div>
+                
+            </div>
+        </>
     );
 };
 
