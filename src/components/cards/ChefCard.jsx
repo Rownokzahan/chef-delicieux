05ae4fdbbd1ed68d9d6ed865615a53b5e217f158
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-04 04:29:42 +0600

    Update ChefCard And Implement Lazy Load

--- src/components/cards/ChefCard.jsx
+++ src/components/cards/ChefCard.jsx
@@ -1,5 +1,6 @@
-import { BsFillSuitHeartFill } from "react-icons/bs";
+import { BsFillHeartFill, } from "react-icons/bs";
 import { TbSoup } from "react-icons/tb";
+import LazyLoad from "react-lazy-load";
 import { Link } from "react-router-dom";
 
 const ChefCard = ({ chef }) => {
@@ -7,31 +8,34 @@ const ChefCard = ({ chef }) => {
     const { id, name, likes, recipes, years_experience, images } = chef;
 
     return (
-        <div className="shadow-md p-4 rounded-lg ">
+        <div className="shadow-md rounded-lg ">
 
-            <div className="text-center">
-                <img className="w-24 rounded-full mx-auto border-2 border-white outline outline-gray-300" src={images.small} alt="" />
+            <div className="h-[200px]">
+                <LazyLoad height={200}>
+                    <img className="w-full h-full rounded-t-lg" src={images.large} alt="" />
+                </LazyLoad>
+            </div>
 
-                <div>
-                    <h3 className="text-xl font-bold mt-4 mb-1 text-gray-800">{name}</h3>
-                    <p className="text-gray-400 font-semibold italic">{years_experience} Years of Experience</p>
+            <div className="text-gray-500 p-4">
+                <div className="flex items-center justify-between">
+                    <h3 className="text-xl font-bold text-gray-800">{name}</h3>
+                    <p className="flex gap-1 text-[18px]">
+                        <BsFillHeartFill className="mt-[5px] text-xl text-red-200" />
+                        <span>{likes}</span>
+                    </p>
+                </div>
+                <p className="mt-3 italic">{years_experience} Years of Cooking Experience</p>
 
-                    <div className="flex justify-between my-4 text-gray-400">
-                        <p className="flex gap-2 items-center">
-                            <TbSoup className="mb-[2px] text-xl" />
-                            {recipes}
-                        </p>
-                        <p className="flex gap-2 items-center">
-                            <BsFillSuitHeartFill className="mt-[2px] text-xl" />
-                            {likes}
-                        </p>
-                    </div>
+                <div className="flex justify-between my-4">
+                    <p className="flex gap-2 items-center">
+                        <TbSoup className="mb-[2px] text-xl" />
+                        {recipes} Recipes
+                    </p>
+                    <Link to={`/chef/${id}`} className="shadow-md hover:bg-ui-red hover:text-white text-ui-red border border-ui-red transform duration-500 text-center px-6 py-2 rounded-3xl font-semibold">View Recipes</Link>
                 </div>
             </div>
 
-            <div className="text-center mb-2">
-                <Link to={`/chef/${id}`} className="shadow-md hover:bg-ui-red hover:text-white text-ui-red border border-ui-red transform duration-500 text-center px-6 py-2 rounded-3xl font-semibold">View Recipes</Link>
-            </div>
+
         </div>
     );
 };
